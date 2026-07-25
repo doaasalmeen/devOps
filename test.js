7d8cd77766548c5e243aa10bf847afb2003a9aec
DoaaSalmen <doaasalmen258@gmail.com>
2026-07-25 11:02:27 +0200

    Update test.js file

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 console.log('Starting test');
 setTimeout(() => {
-    
-}, console.log('wating 3 second ...'), 3000);
-
-console.log('Tests complete');
+   console.log('Tests complete')  
+}, 3000);
+console.log('wating 3 second ...');
